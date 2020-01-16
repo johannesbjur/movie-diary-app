b0d88baa2c39458123811d26930de98598ed8ec2
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-01-16 13:14:51 +0100

    xcode project created and added fonts

---
+++ MovieDiary/MovieDiary/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MovieDiary
+//
+//  Created by Johannes Bjurströmer on 2020-01-16.
+//  Copyright © 2020 Johannes Bjurströmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
