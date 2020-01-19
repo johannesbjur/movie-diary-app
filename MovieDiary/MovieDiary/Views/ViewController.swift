d859abed8fbef83c132e3f1b410c08c7e0a44510
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-01-19 17:21:04 +0100

    constants class for colors added

--- MovieDiary/MovieDiary/Views/ViewController.swift
+++ MovieDiary/MovieDiary/Views/ViewController.swift
@@ -42,16 +42,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         self.tableView.reloadData()
     }
-
+    
     
     func setBGroundToGradient() {
         
         let gradientBackground = CAGradientLayer()
         
+//        gradientBackground.colors = [
+//            UIColor.init( red: 204.0/255.0, green: 43.0/255.0, blue: 94.0/255.0, alpha: 1 ).cgColor,
+//            UIColor.init( red: 117.0/255.0, green: 58.0/255.0, blue: 136.0/255.0, alpha: 1 ).cgColor
+//        ]
         gradientBackground.colors = [
-            UIColor.init( red: 204.0/255.0, green: 43.0/255.0, blue: 94.0/255.0, alpha: 1 ).cgColor,
-            UIColor.init( red: 117.0/255.0, green: 58.0/255.0, blue: 136.0/255.0, alpha: 1 ).cgColor
+            Colors.pink,
+            Colors.purple
         ]
+        
         
         gradientBackground.frame = view.frame
         
