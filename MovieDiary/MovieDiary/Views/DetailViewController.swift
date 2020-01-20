e048165827933b71a08e5b13bdcabd269a16b7c3
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-01-20 13:13:30 +0100

    detail view created

---
+++ MovieDiary/MovieDiary/Views/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  MovieDiary
+//
+//  Created by Johannes Bjurströmer on 2020-01-20.
+//  Copyright © 2020 Johannes Bjurströmer. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var waveBackground: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+
+}
