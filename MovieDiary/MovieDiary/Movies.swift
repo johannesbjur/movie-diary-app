453fdee3673b515fd10f36530909b3b33fcaf9e0
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-02-05 11:37:14 +0100

    movies class created

---
+++ MovieDiary/MovieDiary/Movies.swift
@@ -0,0 +1,31 @@
+//
+//  Movies.swift
+//  MovieDiary
+//
+//  Created by Johannes Bjurströmer on 2020-02-05.
+//  Copyright © 2020 Johannes Bjurströmer. All rights reserved.
+//
+
+import Foundation
+
+
+class Movies {
+    
+    var movies: [Movie] = []
+    
+    
+    init( movies: [Movie] ) {
+        
+        self.movies = movies
+    }
+    
+    func add( movie: Movie ) {
+        
+        self.movies.append( movie )
+    }
+    
+    
+    
+    
+    
+}
