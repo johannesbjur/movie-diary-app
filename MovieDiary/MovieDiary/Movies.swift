1e45d7318c753637381072b92e277c2bb6de4787
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-02-05 13:06:14 +0100

    movies class save functions created

--- MovieDiary/MovieDiary/Movies.swift
+++ MovieDiary/MovieDiary/Movies.swift
@@ -7,14 +7,18 @@
 //
 
 import Foundation
+import Firebase
 
 
 class Movies {
     
-    var movies: [Movie] = []
+    var movies: [Movie]
+    
+    var db: Firestore!
+    var auth: Auth!
     
     
-    init( movies: [Movie] ) {
+    init( movies: [Movie] = [] ) {
         
         self.movies = movies
     }
@@ -24,15 +28,51 @@ class Movies {
         self.movies.append( movie )
     }
     
+    func empty() {
+        
+        self.movies = []
+    }
+    
 //    save movies to database
     func save()  {
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        db = Firestore.firestore()
+        let moviesRef = db.collection( "users" ).document( uid ).collection( "movies" )
+        
+        for movie in movies {
+            
+            moviesRef.addDocument( data: movie.toDict() )
+        }
     }
     
 //    get movies from database with completion statement
     func update() {
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        db = Firestore.firestore()
+        let moviesRef = db.collection( "users" ).document( uid ).collection( "movies" )
+        
+        moviesRef.getDocuments() { ( querySnapshot, err ) in
+            
+            guard let documents = querySnapshot?.documents else {return}
+            
+            self.empty()
+            
+            for document in documents {
+                
+                if let movie = Movie( snapshot: document ) {
+                    
+                    self.add( movie: movie )
+                }
+            }
+        }
+        
     }
+    
+    
     
     
     
